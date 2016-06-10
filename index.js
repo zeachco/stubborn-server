c25ed374644c96ad6db6bab190a0bffff604beae
Ruy Adorno <ruyadorno@hotmail.com>
2016-06-10 18:59:58 -0400

    Fixes issues when a config file is not available

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fileConfig = require('./server/config');
 
 const api = {};
 api.log = require('./server/logger');
-api.start = (config) => fileConfig(config) && require('./server') && api;
+api.start = (config) => fileConfig(config) && require('./server')(config) && api;
 api.stop = () => require('./server/app').process.close() || api;
 
 module.exports = api;
