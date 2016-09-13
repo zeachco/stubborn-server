b60ebc2cc2e0c4d958dffaaf3cf1343fd04da8ba
Olivier Rousseau <zeachco@gmail.com>
2016-09-13 23:35:00 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
