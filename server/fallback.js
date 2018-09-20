87a92ad2f6d2593c77a8b7609328e8fe6194e654
Ruy Adorno <ruyadorno@hotmail.com>
2018-09-19 22:37:43 -0400

    Updated express-http-proxy@1.4.0

--- server/fallback.js
+++ server/fallback.js
@@ -7,17 +7,15 @@ const config = require('./config').get();
 module.exports = app => {
   config.fallbacks.forEach(item => {
     if (item.target) {
-      app.use(item.url, (req, res, next) => {
-        proxy(item.target, {
-          forwardPath: function(req) {
-            return require('url').parse(req.url).path;
-          },
-          decorateRequest: function(req) {
-            req.headers['X-Server-Proxy'] = 'Stubborn-server';
-            return req;
-          }
-        })(req, res, next);
-      });
+      app.use(item.url, proxy(item.target, {
+        proxyReqPathResolver: function(req) {
+          return require('url').parse(req.url).path;
+        },
+        proxyReqOptDecorator: function(proxyReqOpts) {
+          proxyReqOpts.headers['X-Server-Proxy'] = 'Stubborn-server';
+          return proxyReqOpts;
+        }
+      }));
     } else if (item.path) { // We assume the target is a local folder if not an url
       app.use(item.url.replace(/\*$/, ''), express.static(item.path));
     }
