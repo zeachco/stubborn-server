18255cce447be3f4fe6dcfe7ca66efb10ec173f4
Gabriel Bédard Sicé <gbedardsice@upgrade.com>
2019-08-14 15:46:23 -0400

    Only force require modules outside of node_modules

--- server/force-require.js
+++ server/force-require.js
@@ -5,7 +5,12 @@ const originalRequire = Module.prototype.require;
 const forceRequire = () => {
   Module.prototype.require = function hijacked(file) {
     const resolved = Module._resolveFilename(file, this, false);
-    delete require.cache[resolved];
+
+    // Only force require items outside of node_modules since those should never change
+    if (resolved && resolved.indexOf('node_modules') === -1) {
+      delete require.cache[resolved];
+    }
+
     return originalRequire.apply(this, arguments);
   };
 };
